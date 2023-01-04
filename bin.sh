251fe83bb18ecfbfdbb7f3572d9c21c75008874a
ej-shafran <ejshafran@hilma.tech>
2023-01-04 13:06:41 +0200

    Make it work!

---
+++ bin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+path="$1"
+basicname=$(sed -nr '/"name":/ s/.*"name": "(.+)",.*/\1/p' $path/package.json)
+version=$(sed -nr '/"version":/ s/.*"version": "(.+)",.*/\1/p' $path/package.json)
+packagename=${basicname/\@/}
+finalname="${packagename//\//-}-$version.tgz"
+
+echo -e "packing ${GREEN}$path${NC} into ${YELLOW}$finalname${NC}..." &&
+  npm pack $path 2>/dev/null &&
+  echo -e "\ninstalling dependencies, along with ${GREEN}$basicname${NC}..." &&
+  npm i "$finalname" 2>/dev/null &&
+  echo -e "removing ${YELLOW}$finalname${NC}..." &&
+  rm "$finalname";
+
