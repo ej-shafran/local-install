5f6dda10e79b4a1114c9052d50d8345e7096785b
ej-shafran <ejshafran@hilma.tech>
2023-06-29 22:23:05 +0300

    Format file

--- bin.sh
+++ bin.sh
@@ -1,43 +1,105 @@
 #!/usr/bin/env bash
 
+# SETTINGS
+
+trap handle_keyboard_interrupt INT
+set -e
+
+# GLOBALS
+
 SCRIPT_NAME="$(basename "$0")"
-
 SCRIPT_ARGS=()
-
 KEEP_TAR=0
+FILE_NAME=
 
-spin() {
-  local cmd="$*"
+# UTILITY FUNCTIONS
 
-  eval "$cmd 2>/dev/null >/dev/null" &
-
-  local pid="$!"
-  local i=0
-  while [ -d "/proc/$pid" ]; do
-    if [ "$i" -lt 103 ]; then
-      i=$((i + RANDOM % 10 + 1))
-      show_progress "$i" 111
-      sleep 0.7
-    fi
-  done
-  show_progress 111 111
+formatted() {
+  echo -en "\x1b[33m$1\x1b[0m"
 }
 
-usage() {
-  echo ""
-  echo -e "$SCRIPT_NAME - pack and install a local NPM package"
-  echo ""
-  echo "Usage: $SCRIPT_NAME [-k] [path]"
-  echo ""
-  echo "Options:"
-  echo -e "\t-h --help      Show this message"
-  echo -e "\t-k --keep-tar  Don't delete the \`.tar\` file after packing"
+info() {
+  printf "\r  [ \033[00;34mINFO\033[0m ] %b" "$1"
+}
+
+query_user() {
+  printf "\r  [ \033[0;33m?\033[0m ] %b\n" "$1"
+}
+
+success() {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %b\n" "$1"
+}
+
+fail() {
+  printf "\r\033[2K  [ \033[0;31mFAIL\033[0m ] %b\n" "$1"
+  exit
 }
 
 exit_abnormal() {
   usage
   exit 1
 }
+
+# LOADING UTILITIES
+
+progress_bar() {
+    local current="$1"
+    local total="$2"
+
+    local bar_size=40
+    local bar_char_done="#"
+    local bar_char_todo="-"
+    local bar_percentage_scale=2
+
+    percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+
+    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
+    todo=$(bc <<< "scale=0; $bar_size - $done" )
+
+    done_sub_bar=$(printf "%${done}s" | tr " " "$bar_char_done")
+    todo_sub_bar=$(printf "%${todo}s" | tr " " "$bar_char_todo")
+
+    info "npm pack [$done_sub_bar$todo_sub_bar] $percent%"
+}
+
+
+load() {
+  local cmd="$*"
+
+  eval "$cmd 2>/dev/null >/dev/null" &
+
+  local pid="$!"
+  local i=0
+
+  while [ -d "/proc/$pid" ]; do
+    if [ "$i" -lt 103 ]; then
+      i=$((i + RANDOM % 10 + 1))
+      progress_bar "$i" 111
+      sleep 0.7
+    fi
+  done
+
+  progress_bar 111 111
+  echo ""
+}
+
+# USAGE
+
+usage() {
+
+cat << EOF
+$SCRIPT_NAME - pack and install a local NPM package
+
+Usage: $SCRIPT_NAME [-k] [path]
+
+Options:
+  -h --help      Show this message
+  -k --keep-tar  Don't delete the \`tar\` file after packing
+EOF
+
+}
+
+# PARSE OPTIONS
 
 check_long_opt() {
   arg="$1"
@@ -83,97 +145,63 @@ check() {
   done
 }
 
-info() {
-  printf "\r  [ \033[00;34mINFO\033[0m ] %b" "$1"
+# HANDLE KEYBOARD INTERRUPT
+
+handle_keyboard_interrupt() {
+  if [ "$KEEP_TAR" -eq 0 ] && [ "$FILE_NAME" ] && [ -f "$FILE_NAME" ]; then
+    rm "$FILE_NAME"
+  fi
 }
 
-BAR_SIZE=40
-BAR_CHAR_DONE="#"
-BAR_CHAR_TODO="-"
-BAR_PERCENTAGE_SCALE=2
-
-show_progress() {
-    current="$1"
-    total="$2"
-
-    # calculate the progress in percentage 
-    percent=$(bc <<< "scale=$BAR_PERCENTAGE_SCALE; 100 * $current / $total" )
-    # The number of done and todo characters
-    done=$(bc <<< "scale=0; $BAR_SIZE * $percent / 100" )
-    todo=$(bc <<< "scale=0; $BAR_SIZE - $done" )
-
-    # build the done and todo sub-bars
-    done_sub_bar=$(printf "%${done}s" | tr " " "${BAR_CHAR_DONE}")
-    todo_sub_bar=$(printf "%${todo}s" | tr " " "${BAR_CHAR_TODO}")
-
-    # output the bar
-    info "packing: [${done_sub_bar}${todo_sub_bar}] ${percent}%"
-}
-
-
-query_user() {
-  printf "\r  [ \033[0;33m?\033[0m ] %b" "$1"
-}
-
-success() {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %b\n" "$1"
-}
-
-fail() {
-  printf "\r\033[2K  [ \033[0;31mFAIL\033[0m ] %b\n" "$1"
-  exit
-}
+# MAIN
 
 main() {
   check "$@"
 
+  local path
   path="${SCRIPT_ARGS[0]}"
 
   if [ -z "$path" ]; then
-    query_user "Enter the path to pack and install:\n"
+    query_user "enter the path to pack and install:\n"
     read -re path
     echo ""
   fi
 
+  local package_json
   package_json="$path/package.json"
 
   if ! [ -f "$package_json" ]; then
-    fail "no package.json found in $path"
+    fail "no package.json found in $(formatted "\"$path\"")"
     exit 1
   fi
+
+  local package_name
+  local parsed_name
+  local version
 
   package_name=$(sed -nr '/"name":/ s/.*"name": "(.+)",.*/\1/p' "$package_json")
   parsed_name=${package_name/\@/}
   version=$(sed -nr '/"version":/ s/.*"version": "(.+)",.*/\1/p' "$package_json")
-  file_name="${parsed_name//\//-}-$version.tgz"
-  
-  set -e
-  
-  info "packing $path into $file_name\n"
-  spin npm pack "$path"
 
-  echo "\n"
-  info "installing dependencies, along with $package_name...\n"
-  npm i "$file_name" 2>/dev/null | while read -r line; do
+  FILE_NAME="${parsed_name//\//-}-$version.tgz"
+  
+  info "packing $(formatted "\"$path\"") into $(formatted "\"$FILE_NAME\"")\n"
+  load npm pack "$path"
+
+  echo ""
+  info "installing dependencies, along with $(formatted "$package_name")\n"
+  npm install --color=always "$FILE_NAME" 2>&1 | while read -r line; do
     info "$line\n"
   done
 
   if [ "$KEEP_TAR" -eq 0 ]; then
     echo ""
-    info "removing $file_name...\n"
-    rm "$file_name"
+    info "removing $(formatted "\"$FILE_NAME\"")...\n"
+    rm "$FILE_NAME"
   fi
 
   echo ""
-  success "Done!"
+  success "done!"
 }
-
-ctrl_c() {
-  if [ "$KEEP_TAR" -eq 0 ] && [ "$file_name" ] && [ -f "$file_name" ]; then
-    rm "$file_name"
-  fi
-}
-
-trap ctrl_c INT
 
 main "$@"
